00281512850eaa22ac97e91e2292c3cbb123a9db
liuhong <liuhong@idealsee.cn>
2017-03-29 09:14:27 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import VerticalLayout from './Layout/HorizontalLayout';
-import Draggable from './Component/Draggable';
+import logo from './logo.svg';
+import VerticalLayout from './Core/Layout/HorizontalLayout';
+import Draggable from './Core/Component/Draggable';
 
+
+/*
 class App extends Component {
   render() {
     return (
@@ -15,6 +18,25 @@ class App extends Component {
         </div>
       </div>
     );
+  }
+}*/
+
+class App extends Component {
+  render() {
+    return (<div className="App">
+      <div className="header">
+        <div className="inline-style float-left">
+          <img src={logo} width={64} height={64} />
+        </div>
+        <div className="inline-style float-left web-title">Logo</div>
+        <div className="inline-style float-right header-button">
+          <span className="button secondary-button">导出</span>
+        </div>
+        <div className="inline-style float-right header-button">
+          <span className="button primary-button">保存</span>
+        </div>
+      </div>
+    </div>)
   }
 }
 
